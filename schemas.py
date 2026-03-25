38c919fab95bbe76ec4a7b1c5000cd34791dea6c
LucasDio62 <lucasadriel007@gmail.com>
2026-03-24 22:35:26 -0400

    separação dos schemas para o codigo ficar mas limpo

---
+++ schemas.py
@@ -0,0 +1,37 @@
+from uuid import UUID
+from typing import Optional
+from pydantic import BaseModel
+from models import StatusOcorrencia, TipoMidia, TipoPerfil
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class UsuarioCreate(BaseModel):
+    nome: str
+    email: str
+    senha: str 
+    tipo_perfil: TipoPerfil
+
+class UsuarioResponse(BaseModel):
+    id: UUID
+    nome: str
+    email: str
+    tipo_perfil: TipoPerfil
+
+class OcorrenciaCreate(BaseModel):
+    anonimo: bool = False 
+    tipo_incidente: str = "Emergência Geral"
+    descricao: str = "Acionamento rápido via botão de emergência."
+    localizacao: str
+
+class OcorrenciaUpdate(BaseModel):
+    status: Optional[StatusOcorrencia] = None
+    responsavel_id: Optional[UUID] = None
+
+class EvidenciaCreate(BaseModel):
+    url_anexo: str  
+    tipo_midia: TipoMidia
+
+class AtualizacaoCreate(BaseModel):
+    mensagem_acao: str
